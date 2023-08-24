30b63585b4bfa54d60bee4d91cf68220f80ddd40
prtpsk <pratapskofficial@gmail.com>
2023-08-24 09:55:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const PORT = 4000 || process.env.PORT;
+
+const hostName = "localhost";
+const server = http.createServer((req, res, next) => {
+  if (req.url == "/") {
+    res.end("<h1>New Home Page</h1>");
+  } else {
+    res.end("404 page is not found");
+  }
+});
+
+server.listen(PORT, hostName, () => {
+  console.log(`server is running on ${hostName} and ${PORT}`);
+});
